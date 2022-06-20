e29de83f38d5ccd50f976bfe83ced855fed0ee5b
Bilal Ahmed <96869856+bilalahmed-RS@users.noreply.github.com>
2022-06-20 19:29:11 +0500

    Update core_ram.py

--- litex_rs/axi_multi_ram/core_ram.py
+++ litex_rs/axi_multi_ram/core_ram.py
@@ -184,5 +184,5 @@ class axi_ram_2(Module, AutoDoc, AutoCSR):
             i_s_axi_rready=bus.r.ready,
         )
         
-        rtl_path = str(os.path.expanduser('~')) + "/tarball/tarball_20_06/litex_rs/litex_rs/axi_ram/rtl/axi_ram.v"
+        rtl_path = str(os.path.expanduser('~')) + "/litex_instll/litex_rs/litex_rs/axi_ram/rtl/axi_ram.v"
         platform.add_source(rtl_path)
