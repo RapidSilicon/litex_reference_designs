d04fdbe4dda18c5e5b53cfad8df8bef78b05f39f
Florent Kermarrec <florent@enjoy-digital.fr>
2022-07-09 10:20:03 +0200

    example_designs: Update axi_ram/helloworld gemini/litex_sim_rs.

--- example_designs/vexriscv_axi_ram/gemini.py
+++ example_designs/vexriscv_axi_ram/gemini.py
@@ -9,15 +9,13 @@ from litex.build.io import CRG
 from litex.build.generic_platform import Pins, Subsignal
 from litex.build.osfpga import OSFPGAPlatform
 
-from Core_IPs.AXI_GPIO.core_axi4gpio import gpio_axi
-from Core_IPs.AXI_RAM.core_ram import axi_ram
+from litex_rs.cores.axi_gpio import AXIGPIO
+from litex_rs.cores.axi_ram  import AXIRAM
 
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 
 from litex.soc.integration.soc import *
-
-
 
 # Platform ---------------------------------------------------------------------------------
 
@@ -31,24 +29,23 @@ _io = [
         Subsignal("rx", Pins(1)),
     ),
     ("gpio", 0,
-    Subsignal("oe", Pins(32)),
-    Subsignal("o",  Pins(32)),
-    Subsignal("i",  Pins(32)),
+        Subsignal("oe", Pins(32)),
+        Subsignal("o",  Pins(32)),
+        Subsignal("i",  Pins(32)),
     ),
 ]
 
 class Platform(OSFPGAPlatform):
-    def __init__(self, toolchain="foedag", device="mpw1"):
+    def __init__(self, toolchain="raptor", device="gemini"):
         OSFPGAPlatform.__init__(self, device=device, toolchain=toolchain, io=_io)
 
 # BaseSoC ------------------------------------------------------------------------------------------
 
 class BaseSoC(SoCCore):
     def __init__(self, platform, 
-        sys_clk_freq=int(10e6), 
-        axigpio=False, 
-        axiram=False, 
-        multiaxiram=False, 
+        sys_clk_freq  = int(10e6),
+        with_axi_gpio = False,
+        with_axi_ram  = False,
         **kwargs):
         
         # CRG --------------------------------------------------------------------------------------
@@ -58,43 +55,47 @@ class BaseSoC(SoCCore):
         SoCCore.__init__(self, platform, sys_clk_freq, ident="LiteX Test SoC on OS-FPGA", **kwargs)
 
 
-        # AXI GPIO------------------------------------------------------------------
-        if axigpio:
-            self.submodules.gpio_axi = gpio_axi(platform, platform.request("gpio"))
-            self.bus.add_slave(name="gpio_axi", slave=self.gpio_axi.bus, region=SoCRegion(
+        # AXI GPIO ---------------------------------------------------------------------------------
+        if with_axi_gpio:
+            self.submodules.axi_gpio = AXIGPIO(platform, platform.request("gpio"))
+            self.bus.add_slave(name="axi_gpio", slave=self.axi_gpio.bus, region=SoCRegion(
                 origin = 0xf0020000,
-                size = 1024,
+                size   = 1024,
                 cached = False,
-                ))  
+                )
+            )
 
-        # AXI RAM---------------------------------------------------------------------------------------
+        # AXI RAM-----------------------------------------------------------------------------------
         if axiram:
-            self.submodules.axi_ram = axi_ram(platform, pads=None)
+            self.submodules.axi_ram = AXIRAM(platform, pads=None)
             self.bus.add_slave(name="axi_ram", slave=self.axi_ram.bus, region=SoCRegion(
                 origin = 0x50000000,
                 size   = 1024, 
-                cached = True, ))
+                cached = True,
+                )
+            )
 
 # Build --------------------------------------------------------------------------------------------
 
 def main():
     from litex.soc.integration.soc import LiteXSoCArgumentParser
-    parser = LiteXSoCArgumentParser(description="LiteX Test SoC on OS-FPGA")
+    parser = LiteXSoCArgumentParser(description="LiteX Test SoC on OS-FPGA.")
     target_group = parser.add_argument_group(title="Target options")
-    target_group.add_argument("--build",            action="store_true",   help="Build design.")
-    target_group.add_argument("--toolchain",        default="raptor",      help="FPGA toolchain.")
-    target_group.add_argument("--device",           default="gemini",      help="FPGA device.")
-    target_group.add_argument("--axigpio",          action="store_true",   help="AXI based 32 bit GPIO")
-    target_group.add_argument("--axiram",           action="store_true",   help="AXI based RAM")
+    target_group.add_argument("--build",         action="store_true", help="Build design.")
+    target_group.add_argument("--toolchain",     default="raptor",    help="FPGA toolchain.")
+    target_group.add_argument("--device",        default="gemini",    help="FPGA device.")
+    target_group.add_argument("--with-axi-gpio", action="store_true", help="Add AXI-GPIO (32-bit) to design.")
+    target_group.add_argument("--with-axi-ram",  action="store_true", help="Add AXI-RAM to design.")
     builder_args(parser)
     soc_core_args(parser)
     args = parser.parse_args()
 
     platform = Platform(toolchain=args.toolchain, device=args.device)
     soc      = BaseSoC(platform,
-                axigpio  = args.axigpio,
-                axiram   = args.axiram,
-                **soc_core_argdict(args))
+        with_axi_gpio = args.with_axi_gpio,
+        with_axi_ram  = args.with_axi_ram,
+        **soc_core_argdict(args)
+    )
                 
     builder  = Builder(soc, **builder_argdict(args))
     if args.build:
