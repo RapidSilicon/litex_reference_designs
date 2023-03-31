49b40eed578c9a65352d8221198788f8079e7a64
Sarmad Salman <ssalman@rslpt09.rapidsilicon.local>
2023-03-31 15:34:05 +0500

    slight modifications and corrections

--- rtl_designs/vexriscv_axi_cdma/sim/testbench.v
+++ rtl_designs/vexriscv_axi_cdma/sim/testbench.v
@@ -1,6 +1,5 @@
 `timescale 1ns/1ps 
 
-
 module Tb;
     reg   clock;
     reg   reset;
@@ -9,22 +8,14 @@ module Tb;
         clock = 1'b0;
         #5;
         reset = 1'b0;
-//        #10;
-//        reset =1'b1;
-//        #10;
-//        reset = 1'b0;
     end
-    always  #(5)     clock = !clock;
+    always  #(2.5)     clock = !clock;
 
         initial begin
             $dumpfile("tb.vcd");
             $dumpvars;
             #7000  $finish;
             $display("SoC Simulation Completed");
-        end
-        initial begin
-        $fsdbDumpfile("waves.fsdb");
-        $fsdbDumpvars(0,"+struct","+mda","+all");
         end
     vex_soc soc(.clk(clock),
                 .reset(reset));
